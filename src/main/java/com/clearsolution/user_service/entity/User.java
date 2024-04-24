82923dbc23785ec2ed6d3f6292088868dd62fefa
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2024-04-24 23:24:52 +0300

    Added user entity and repository

---
+++ src/main/java/com/clearsolution/user_service/entity/User.java
@@ -0,0 +1,53 @@
+package com.clearsolution.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", length = 64, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 64, nullable = false)
+    private String lastName;
+
+    @Column(name = "email", length = 64, nullable = false, unique = true)
+    private String email;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "birth_date", nullable = false)
+    private LocalDateTime birthDate;
+
+    @Column(name = "address", length = 128)
+    private String address;
+
+    @Column(name = "phone_number", length = 32, unique = true)
+    private String phoneNumber;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
